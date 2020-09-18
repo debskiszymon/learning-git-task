e5c10c1abe8c6239ac86587bf42e4222f84ffcbf
debskiszymon <debski.szymon@gmail.com>
2020-09-18 18:33:10 +0200

    created dict

--- main.py
+++ main.py
@@ -3,14 +3,6 @@ shopping_list = {
 "warzywniak": ["pomidory", "cebula", "marchew", "ziemniaki"]
 }
 
-#shopping_list1 = {}
-
-#for key, values in shopping_list.items():
-#    for z in values:
-#        shopping_list[key].append(z)
-#
-#print(shopping_list)
-
 count = 0
 for key, value in shopping_list.items():
     count += len(value)
@@ -18,4 +10,5 @@ for key, value in shopping_list.items():
 for key, values in shopping_list.items():
     print(f"Idę do {key.capitalize()} i kupuję tam {[value.capitalize() for value in values]}.")
 print(f"W sumie kupuję {count} produktów.")
+zzz
 
